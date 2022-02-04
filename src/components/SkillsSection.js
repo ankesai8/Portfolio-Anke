54db0d7592a1e3af293b8df5d009af2530bb2972
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-02-04 17:41:09 +0530

    Update SkillsSection.js

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -75,7 +75,7 @@ const Text = styled.h2`
 @media (max-width: 768px){
     margin-top: 1rem;  
     margin-bottom: 10vh; 
-    }
+    } 
 @media (max-width: 480px){
     margin-top: 2vh;
     margin-bottom: 7vh;
